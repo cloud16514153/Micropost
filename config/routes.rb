35f5d632de2385b4d68334ebe27bd8c7c2e6a217
cloud16514153 <jitsukeweb@gmail.com>
2022-12-31 05:31:53 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+  get 'microposts/create'
+  get 'microposts/destroy'
   root to: 'toppages#index'
-  
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
+
+  resources :microposts, only: [:create, :destroy]
 end
